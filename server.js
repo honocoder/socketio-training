01f46b2b6e840ed0f26b26d3279f728c53e43d09
jimymltta <marlettajimy@gmail.com>
2022-04-15 12:14:00 +0200

    implemented dynamic message, username and time displaying in the DOM.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require("express");
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,22 +11,24 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
+const botName = 'ChatCord Bot';
+
 // Run when a client connects
 io.on('connection', socket => {
     // Welcome current user
-    socket.emit('message', 'Welcome to ChatCord!');
+    socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
 
     // Broadcast when a user connect
-    socket.broadcast.emit('message', 'A user has joined the room');
+    socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the room'));
 
     // Runs when client disconnects
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the room');
+        io.emit('message', formatMessage(botName, 'A user has left the room'));
     });
 
     // Listen for chatMessage
     socket.on('chatMessage', msg => {
-        io.emit('message', msg)
+        io.emit('message', formatMessage("User", msg))
     });
 });
 
